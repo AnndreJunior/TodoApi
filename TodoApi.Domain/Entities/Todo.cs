7312458925fe3dbf635da57eb3c05226489a7df6
AnndreJunior <anndre.junior15@gmail.com>
2024-03-16 18:28:51 -0300

    feat: create todo entity

---
+++ TodoApi.Domain/Entities/Todo.cs
@@ -0,0 +1,22 @@
+namespace TodoApi.Domain.Entities;
+
+public class Todo
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; }
+    public bool IsComplete { get; private set; }
+
+    public Todo(string title)
+    {
+        ValidateTodoTitle(title);
+
+        Id = Guid.NewGuid();
+        Title = title;
+    }
+
+    private void ValidateTodoTitle(string title)
+    {
+        if (title.Length == 0 || title == null)
+            throw new InvalidDataException("Informe sua tarefa");
+    }
+}
